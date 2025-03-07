8f4b54420d7629a9873b32b1b48886d41f29932c
paulweezydesign <minksy13@gmail.com>
2025-03-07 12:58:56 -0700

    Add sign-up page

---
+++ pages/signup.tsx
@@ -0,0 +1,9 @@
+import { SignUpForm } from "@/components/ui/sign-up-form";
+
+export default function SignUpPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4 bg-background">
+      <SignUpForm />
+    </div>
+  );
+}
